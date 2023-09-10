e4de81a30b7f970529d5d0816d0538bdf3f0fa79
Pedro Oriani <pedro.oriani@gmail.com>
2023-09-10 12:32:12 -0300

    build: joi.date

--- src/schemas/flights.schemas.js
+++ src/schemas/flights.schemas.js
@@ -1,7 +1,9 @@
-import Joi from "joi";
+import DateExtension from '@joi/date';
+import JoiImport from 'joi';
+const Joi = JoiImport.extend(DateExtension);
 
 export const flightSchema = Joi.object({
     origin: Joi.number().integer().required(),
     destination: Joi.number().integer().required(),
-    date: Joi.date().required()
+    date: Joi.date().format('DD-MM-YYYY').utc().required()
 })
